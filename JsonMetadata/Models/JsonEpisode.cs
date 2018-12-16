1d9566327e63f5ee23a24498c0f40f536a2c8258
Henry Buckle <h.buckle@outlook.com>
2018-12-16 09:37:32 +0000

    add episode saver

---
+++ JsonMetadata/Models/JsonEpisode.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+
+namespace JsonMetadata.Models
+{
+  [DataContract]
+  public class JsonEpisode : JsonObject
+  {
+    [DataMember(Name = "title", Order = 501)]
+    public string title { get; set; }
+
+    [DataMember(Name = "sorttitle", Order = 502)]
+    public string sorttitle { get; set; }
+
+    // [DataMember(Name = "dateadded", Order = 503)]
+    // public DateTime dateadded { get; set; }
+
+    [DataMember(Name = "seasonnumber", Order = 504)]
+    public int? seasonnumber { get; set; }
+
+    [DataMember(Name = "episodenumber", Order = 505)]
+    public int? episodenumber { get; set; }
+
+    [DataMember(Name = "communityrating", Order = 506)]
+    public float? communityrating { get; set; }
+
+    [DataMember(Name = "overview", Order = 507)]
+    public string overview { get; set; }
+
+    [DataMember(Name = "releasedate", Order = 508)]
+    public DateTime? releasedate { get; set; }
+
+    [DataMember(Name = "year", Order = 509)]
+    public int? year { get; set; }
+
+    [DataMember(Name = "parentalrating", Order = 510)]
+    public int? parentalrating { get; set; }
+
+    [DataMember(Name = "customrating", Order = 511)]
+    public string customrating { get; set; }
+
+    [DataMember(Name = "originalaspectratio", Order = 512)]
+    public string originalaspectratio { get; set; }
+
+    // [DataMember(Name = "3dformat", Order = 513)]
+    // public string threedformat { get; set; }
+
+    [DataMember(Name = "imdbid", Order = 514)]
+    public string imdbid { get; set; }
+
+    [DataMember(Name = "tvdbid", Order = 515)]
+    public string tvdbid { get; set; }
+
+    [DataMember(Name = "genres", Order = 516)]
+    public string[] genres { get; set; }
+
+    [DataMember(Name = "people", Order = 517)]
+    public List<JsonCastCrew> people { get; set; }
+
+    [DataMember(Name = "studios", Order = 518)]
+    public string[] studios { get; set; }
+
+    [DataMember(Name = "tags", Order = 519)]
+    public string[] tags { get; set; }
+
+    [DataMember(Name = "lockdata", Order = 520)]
+    public bool lockdata { get; set; }
+  }
+}
---
+++ JsonMetadata/Savers/EpisodeJsonSaver.cs
@@ -0,0 +1,121 @@
+using MediaBrowser.Controller.Configuration;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Serialization;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Linq;
+using MediaBrowser.Controller.IO;
+using MediaBrowser.Model.IO;
+using JsonMetadata.Configuration;
+using JsonMetadata.Models;
+
+namespace JsonMetadata.Savers
+{
+  public class EpisodeJsonSaver : BaseJsonSaver
+  {
+    public EpisodeJsonSaver(IFileSystem fileSystem, IServerConfigurationManager configurationManager, ILibraryManager libraryManager, IUserManager userManager, IUserDataManager userDataManager, ILogger logger) : base(fileSystem, configurationManager, libraryManager, userManager, userDataManager, logger)
+    {
+    }
+    protected override string GetLocalSavePath(BaseItem item)
+    {
+      return Path.ChangeExtension(item.Path, ".json");
+    }
+
+    protected override string GetRootElementName(BaseItem item)
+    {
+      return "episodedetails";
+    }
+
+    public override bool IsEnabledFor(BaseItem item, ItemUpdateType updateType)
+    {
+      if (!item.SupportsLocalMetadata)
+      {
+        return false;
+      }
+      return item is Episode && updateType >= MinimumUpdateType;
+    }
+
+    protected override JsonObject SerializeItem(BaseItem item, IServerConfigurationManager options, ILibraryManager libraryManager)
+    {
+      var hasAspectRatio = item as IHasAspectRatio;
+      var episode = item as Episode;
+      var output = new JsonEpisode()
+      {
+        id = item.InternalId,
+        title = item.Name ?? string.Empty,
+        sorttitle = item.ForcedSortName ?? string.Empty,
+        // dateadded = item.DateCreated.LocalDateTime,
+        seasonnumber = episode.ParentIndexNumber,
+        episodenumber = episode.IndexNumber,
+        communityrating = item.CommunityRating,
+        overview = item.Overview ?? string.Empty,
+        releasedate = item.PremiereDate.HasValue ? item.PremiereDate.Value.LocalDateTime : new DateTime?(),
+        year = item.ProductionYear,
+        parentalrating = item.GetParentalRatingValue(),
+        customrating = item.CustomRating ?? string.Empty,
+        originalaspectratio = hasAspectRatio != null ? hasAspectRatio.AspectRatio : string.Empty,
+        imdbid = item.GetProviderId(MetadataProviders.Imdb) ?? string.Empty,
+        tvdbid = item.GetProviderId(MetadataProviders.Tvdb) ?? string.Empty,
+        lockdata = item.IsLocked,
+      };
+      output.genres = item.Genres;
+      var people = item.SupportsPeople ? libraryManager.GetItemPeople(new InternalPeopleQuery
+      {
+        ItemIds = new[] { item.InternalId },
+        EnableImages = options.GetJsonConfiguration().SaveImagePathsInNfo,
+        EnableGuids = true,
+        EnableIds = true
+      }) : new List<PersonInfo>();
+      output.people = new List<JsonCastCrew>();
+      foreach (var person in people)
+      {
+        var personitem = libraryManager.GetItemById(person.Id);
+        var image = person.ImageInfos.FirstOrDefault(i => i.Type == ImageType.Primary);
+        var jsonperson = new JsonCastCrew();
+        jsonperson.thumb = image != null ? GetImagePathToSave(image, libraryManager, options) : string.Empty;
+        jsonperson.name = person.Name ?? string.Empty;
+        jsonperson.id = person.Id;
+        jsonperson.tmdbid = personitem.GetProviderId(MetadataProviders.Tmdb) ?? string.Empty;
+        jsonperson.imdbid = personitem.GetProviderId(MetadataProviders.Imdb) ?? string.Empty;
+        jsonperson.type = person.Type.ToString();
+        switch (person.Type)
+        {
+          case PersonType.Actor:
+            jsonperson.role = person.Role ?? string.Empty;
+            break;
+          default:
+            jsonperson.role = string.Empty;
+            break;
+        }
+        output.people.Add(jsonperson);
+      }
+      output.studios = item.Studios;
+      output.tags = item.Tags;
+      return output;
+    }
+
+    protected override List<string> GetTagsUsed(BaseItem item)
+    {
+      var list = base.GetTagsUsed(item);
+      list.AddRange(new string[]
+      {
+        "aired",
+        "season",
+        "episode",
+        "episodenumberend",
+        "airsafter_season",
+        "airsbefore_episode",
+        "airsbefore_season",
+        "displayseason",
+        "displayepisode",
+      });
+      return list;
+    }
+  }
+}
