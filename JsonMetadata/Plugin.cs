9b8446f4a3cd0f6ffa87f980a29bba402a00b5ea
Henry Buckle <h.buckle@outlook.com>
2019-05-24 21:11:03 +0100

    formatting

--- JsonMetadata/Plugin.cs
+++ JsonMetadata/Plugin.cs
@@ -7,64 +7,64 @@ using System.Collections.Generic;
 
 namespace JsonMetadata
 {
-    public class Plugin : BasePlugin, IHasThumbImage, IHasWebPages
+  public class Plugin : BasePlugin, IHasThumbImage, IHasWebPages
+  {
+    private Guid _id = new Guid("4678ee1d-91c7-47c3-9da6-fe48d2695e94");
+    public override Guid Id
     {
-        private Guid _id = new Guid("4678ee1d-91c7-47c3-9da6-fe48d2695e94");
-        public override Guid Id
-        {
-            get { return _id; }
-        }
-
-        public override string Name
-        {
-            get { return StaticName; }
-        }
-
-        public static string StaticName
-        {
-            get { return "Json Metadata"; }
-        }
-
-        public override string Description
-        {
-            get
-            {
-                return "Json metadata support";
-            }
-        }
-
-        public Stream GetThumbImage()
-        {
-            var type = GetType();
-            return type.Assembly.GetManifestResourceStream(type.Namespace + ".thumb.png");
-        }
-
-        public ImageFormat ThumbImageFormat
-        {
-            get
-            {
-                return ImageFormat.Png;
-            }
-        }
-
-        public IEnumerable<PluginPageInfo> GetPages()
-        {
-            return new[]
-            {
-                new PluginPageInfo
-                {
-                    Name = "json",
-                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.json.html",
-                    EnableInMainMenu = true,
-                    MenuSection = "server",
-                    MenuIcon = "notes"
-                },
-                new PluginPageInfo
-                {
-                    Name = "jsonjs",
-                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.json.js"
-                }
-            };
-        }
+      get { return _id; }
     }
+
+    public override string Name
+    {
+      get { return StaticName; }
+    }
+
+    public static string StaticName
+    {
+      get { return "Json Metadata"; }
+    }
+
+    public override string Description
+    {
+      get
+      {
+        return "Json metadata support";
+      }
+    }
+
+    public Stream GetThumbImage()
+    {
+      var type = GetType();
+      return type.Assembly.GetManifestResourceStream(type.Namespace + ".thumb.png");
+    }
+
+    public ImageFormat ThumbImageFormat
+    {
+      get
+      {
+        return ImageFormat.Png;
+      }
+    }
+
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+      return new[]
+      {
+        new PluginPageInfo
+        {
+            Name = "json",
+            EmbeddedResourcePath = GetType().Namespace + ".Configuration.json.html",
+            EnableInMainMenu = true,
+            MenuSection = "server",
+            MenuIcon = "notes"
+        },
+        new PluginPageInfo
+        {
+            Name = "jsonjs",
+            EmbeddedResourcePath = GetType().Namespace + ".Configuration.json.js"
+        }
+      };
+    }
+  }
 }
